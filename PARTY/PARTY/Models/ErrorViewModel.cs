ffc3ab1eb3cc7ced1ab45d8c3da2dc88fad27ad2
mazidan77 <moga4arab@gmail.com>
2023-01-27 02:31:06 +0200

    ADD LAYOUT

---
+++ PARTY/PARTY/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PARTY.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
